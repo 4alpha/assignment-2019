11b39fca7f906aa9e40d48e6929f08cada22a174
harshada-4alpha <harshada@4alpha.in>
2019-02-13 12:04:43 +0530

    modified demo.sql

--- Harshada/script/demo.sql
+++ Harshada/script/demo.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS admin(username varchar(20) NOT NULL,password varchar(
 INSERT INTO admin (username, password) VALUES('admin', 'admin'); 
 
 CREATE TABLE IF NOT EXISTS passport (
-	email varchar(30) NOT NULL,
+	email varchar(30) NOT NULL PRIMARY KEY UNIQUE,
 	passwd varchar(30) NOT NULL,
 	application_for varchar(20),
 	type_of_application varchar(10),
@@ -17,4 +17,7 @@ CREATE TABLE IF NOT EXISTS passport (
 	pan varchar(10) NOT NULL,
 	address varchar(50) NOT NULL,
 	mobile_no varchar(10) NOT NULL);
+
+	INSERT INTO passport (email, passwd, application_for, type_of_application, booklet, name, surname, picture, gender, place, birth_date, pan, address, mobile_no)
+	VALUES ('harshada@gmail.com','Harsh@10','Fresh Passport','normal','36','harsh','jadhav','girl.jpeg','female','no','1995/10/31','PUKJL2343K','Pune','9834589876');
 
