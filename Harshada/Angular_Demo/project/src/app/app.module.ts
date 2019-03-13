802c147a77f78ac995ffed6975c84ad9907cf580
harshada-4alpha <harshada@4alpha.in>
2019-03-13 15:30:09 +0530

    Angular routing demo

---
+++ Harshada/Angular_Demo/project/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+
+
+const appRoutes: Routes = [ 
+ { path: 'imageFirst', component: FirstComponent  },
+ 
+ { path: 'imageSecond', component: SecondComponent  },
+
+ { path: '', component: HomeComponent},
+
+ { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    PageNotFoundComponent,
+    HomeComponent,  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
