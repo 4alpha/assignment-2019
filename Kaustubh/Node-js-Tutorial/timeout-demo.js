bbb9c47e32027f77f00b16bde9af98616cb38961
kaustubhkz <kaustubh@4alpha.in>
2019-03-14 18:06:02 +0530

    Few timer events

--- Kaustubh/Node-js-Tutorial/timeout-demo.js
+++ Kaustubh/Node-js-Tutorial/timeout-demo.js
@@ -1,10 +1,47 @@
+/*
+
+//Timeout event
 setTimeout(() =>
 {
 console.log("Timeout for 5s");
 },5000);
 
+//Immediate event execution
 setImmediate(()=>
 {
     console.log("Immediate task");
 }
 );
+*/
+
+//Using setInterval() which starts something and clearInterval() to stop the event
+/*
+var count=0;
+
+var intervalCount=setInterval(()=>
+{
+    count++;
+    console.log(count,' has passed');
+
+    if(count===5){
+        console.log("Exiting now");
+        clearInterval(intervalCount);
+    }
+},1000)
+*/
+
+//Let's use setInterval() and clearInterval() for another event
+
+var count=0;
+
+var interval=setInterval(()=>
+{
+    count++;
+    console.log("Hello there ", count);
+
+    if(count==10)
+    {
+        console.log("Goodbye");
+        clearInterval(interval);
+    }
+},1000);
