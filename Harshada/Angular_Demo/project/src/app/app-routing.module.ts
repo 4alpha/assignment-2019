e7e38f9b729f336a7a59aee8e8db49a132eae050
harshada-4alpha <harshada@4alpha.in>
2019-03-15 15:41:35 +0530

    ppt

--- Harshada/Angular_Demo/project/src/app/app-routing.module.ts
+++ Harshada/Angular_Demo/project/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  { path: 'imageFirst', component: FirstComponent  },
+ 
+ { path: 'imageSecond', component: SecondComponent  },
+
+ { path: '', component: HomeComponent},
+
+ { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+  { enableTracing: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Harshada/Angular_Demo/project/src/app/app.module.ts
+++ Harshada/Angular_Demo/project/src/app/app.module.ts
@@ -31,10 +31,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
