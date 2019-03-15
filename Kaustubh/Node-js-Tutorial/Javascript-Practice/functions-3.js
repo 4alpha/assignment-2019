bb2f7cf41afbde88b972d28e52d614c07ec15a3a
kaustubhkz <kaustubh@4alpha.in>
2019-03-15 18:35:03 +0530

    This week's work

---
+++ Kaustubh/Node-js-Tutorial/Javascript-Practice/functions-3.js
@@ -0,0 +1,29 @@
+/*function newConcat(separators)
+{
+  var result='';
+  var i;
+  for(i=0;i<arguments.length;i++)
+    {
+      result +=arguments[i];
+    }
+  return result;
+}
+*/
+/*function foo(i) {
+  if (i < 0)
+    return;
+  console.log('begin: ' + i);
+  foo(i - 1);
+  console.log('end: ' + i);
+}
+foo(3);
+//  console.log(newConcat('Hello','there','u','love','it'));
+//Here I've missed in giving first parameter as , , that's why my o/p was printing
+// thereHellouHelloloveHelloitHello
+// So I've given comma operator in beginning which finally prints o/p 
+//Hello,there,u,love,it,
+
+/*
+Exception: ReferenceError: newConcat is not defined
+@Scratchpad/2:20:1
+*/
