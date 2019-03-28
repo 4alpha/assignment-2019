4236d50be2708a91aebca5e5d1566cf191287a85
shripad-4alpha <shripad@4alpha.in>
2019-03-28 17:27:25 +0530

    Completed Section 5 of React Course

--- Shripad/React/seasons/src/index.js
+++ Shripad/React/seasons/src/index.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return <div>Hi There !</div>;
+class App extends React.Component {
 
-};
+  constructor(props){
+      super(props);
+        
+        this.state= {lat : null, errMessage: ''};
+
+        window.navigator.geolocation.getCurrentPosition(
+          (position) => {
+            this.setState({ lat : position
+            .coords.latitude});
+          },
+          (err) => {
+            this.setState({errMessage : err.message})
+
+          }
+        );
+  }
+  
+  
+  
+  render(){
+      if(this.state.errMessage && !this.state.lat){
+        return  <div>
+                 Error ! {this.state.errMessage}
+                </div>;
+       }
+
+       if(!this.state.errMessage && this.state.lat){
+        return  <div>
+                 Latitude is {this.state.lat}
+                </div>;
+       }
+
+    return  <div>Loading... </div>;
+
+  }
+}
 
 ReactDOM.render(
   <App />,
