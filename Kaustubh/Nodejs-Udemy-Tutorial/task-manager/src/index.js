d5ed2d15429e0f8937f21c37c071c532544a15dd
kaustubhkz <kaustubh@4alpha.in>
2019-04-09 18:14:16 +0530

    Changes wrt async-await

--- Kaustubh/Nodejs-Udemy-Tutorial/task-manager/src/index.js
+++ Kaustubh/Nodejs-Udemy-Tutorial/task-manager/src/index.js
@@ -13,95 +13,119 @@ const app = express();
 app.use(express.json());
 //Using routes for POST method
 
-app.post('/users',(req,res)=>{
-       // console.log(req.body);         
+app.post('/users',async(req,res)=>{          
        const user=new User.newUser(req.body);
-       user.save().then(()=>{       
-        //    201=Created
+    //    user.save().then(()=>{       
+    //     //    201=Created
+    //     res.status(201).send(user);
+    //    }).catch((error)=>{
+    //     //Shorthand expression  
+    //     res.status(400).send('Error details\n',error);
+    //     //    res.send('Error details\n',error);
+    //    });
+    try {
+        await user.save();
         res.status(201).send(user);
-       }).catch((error)=>{
-        //Shorthand expression  
-        res.status(400).send('Error details\n',error);
-        //    res.send('Error details\n',error);
-       });    
+        //The below can also work
+        //  const newUser= await user.save();
+        // res.status(201).send(newUser);
+    } catch (error) {
+        res.status(400).send('Error details\n'+error);
+    }    
 });
 // Creating another route to display Cars and it's details
-app.post('/cars',(req,res)=>{
+app.post('/cars',async(req,res)=>{
     const newCar=new Car.createNewCar(req.body);
-    newCar.save().then(()=>{
-        //201=Created
-        res.status(201).send(newCar);
-    }).catch((error)=>{
-        res.send(400).send('Error occured check the following ',error);
-    })
-})
 
-// A route for finding user by ID
-app.get('/users/:id',(req,res)=>{
-    if(!req.params.id)
-    res.status(400).send("Please insert ID");
-    else
-    {
-        // const result=User.findUserById(req.params.id);
-        // if(result===404)
-        // res.status(result).send("No user found");
-        // else if(result===500)
-        // res.status(result).send("Error at server");
-        // else
-        // res.status(202).send(result);
-        // Don't give () next to newUser
-       User.newUser.findById(req.params.id).then((userDetails)=>{
-            if(!userDetails)
-            res.status(404).send("No User found");
-            else{
-                res.status(202).send(userDetails);
-            }
-        }).catch((err)=>{
-            res.status(500).send("Server side error Invalid string provided");
-            console.log(err);
-            
-        })
+    try {
+        await newCar.save();
+        res.status(201).send(newCar);
+    } catch (error) {
+        res.send(400).send('Error occured check the following ');
     }
 })
 
-
 //Creating a route to return user details based on user name
-app.get('/users',(req,res)=>{
-    if(!req.body)
-    res.status(400).send("Please insert username");
-    else
-    {
-        const userName=req.body;
-        const fetchUser=User.findUser(userName);
-        if(fetchUser===undefined)
-        {
-            res.status(404).send("No result found");
-        }
+app.get('/users',async(req,res)=>{
+   try {
+       const users=await User.newUser.find({});
+       res.status(201).send(users);
+   } catch (error) {
+       res.status(400).send('Error while getting Users '+error);
+   }
+})
+
+// A route for finding user by ID
+app.get('/users/:id',async(req,res)=>{               
+    // Don't give () next to User.newUser
+    
+    try {
+        const userDetails=await User.newUser.findById(req.params.id);
+        if(!userDetails)
+        res.status(404).send('No user found by ID');
         else
-        {
-            res.send(fetchUser);
-        }
+        res.status(200).send(userDetails);    
+    } catch (error) {
+        res.status(500).send("Server Error, Invalid url parameter"+error);
+    }
+
+})
+
+//A route to update User details using  ID by PATCH method
+app.patch('/users/:id',async (req,res)=>{
+    try {
+        // To avoid or to send flawed status code even when not updated from schema, we add certain filters
+        const updates=Object.keys(req.body);
+        const validUpdates=['name','age','email','password'];
+
+        const isValidUpdates=updates.every((update)=>validUpdates.includes(update));
+
+        if(!isValidUpdates)
+        return res.status(400).send('No field present in model');
+        //In optional part, new=true ensures that returned document must be updated
+        //  rather than original document
+        // Default value is false
+        // runValidators=true allows updated value should be according to schema
+        const userUpdatedData=await User.newUser.findByIdAndUpdate(req.params.id,req.body,{new: true,runValidators: true});
+        if(!userUpdatedData)
+        return res.status(404).send('User not found by ID');
+        res.status(200).send(userUpdatedData);
+    } catch (error) {
+        res.status(500).send("Server error while updating User data"+error);
     }
 })
 
 //Create an endpoint to fetch all cars
 
-app.get('/cars',(req,res)=>{
-    Car.createNewCar.find().then((carDetails)=>{
-        res.status(202).send(carDetails);
-    })
+app.get('/cars',async(req,res)=>{
+    // Car.createNewCar.find().then((carDetails)=>{
+    //     res.status(202).send(carDetails);
+    // })
+    try {
+        const carDetails=await Car.createNewCar.find({});
+        if(!carDetails)
+        res.status(404).send('No car found by ID');
+        else
+        res.status(200).send(carDetails);
+    } catch (error) {
+        res.status(500).send('Error occured at Server side '+error);
+    }
 })
 
 //Create an endpoint to fetch car by its ID
 
-app.get('/cars/:id',(req,res)=>{
-    Car.createNewCar.findById(req.params.id).then((carDetails)=>{
-        if(!carDetails)
-        res.status(404).send("No car found by this ID");
-        else
+app.get('/cars/:id',async(req,res)=>{    
+    // Get details of Car by ID
+    try
+    {
+    const carDetails=await Car.createNewCar.findById(req.params.id);
+    if(!carDetails)
+    res.status(404).send("No Car found by ID");
+    else
         res.status(202).send(carDetails);
-    }).catch(()=>{
-        res.status(500).send("Check your Car ID in URL");
-    })
+    }
+    catch(error){
+        res.status(500).send("Something went wrong while getting your Car by it\'s ID "+error);
+    }
 })
 app.listen(portNumber, () => console.log('Server listening at port ',portNumber))
