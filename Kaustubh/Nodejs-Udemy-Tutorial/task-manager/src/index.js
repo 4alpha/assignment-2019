2cf949bdf56951fbaafea8527c99896b79b99f65
kaustubhkz <kaustubh@4alpha.in>
2019-04-12 15:56:59 +0530

    Code rearrangement using beautify

--- Kaustubh/Nodejs-Udemy-Tutorial/task-manager/src/index.js
+++ Kaustubh/Nodejs-Udemy-Tutorial/task-manager/src/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 //Include Mongoose to conncect to DB
 require("./db/mongoose");
 
-const portNumber=process.env.PORT || 9000;
+const portNumber = process.env.PORT || 9000;
 // Import user's route
-const userRouters=require('../src/routers/userRouter');
+const userRouters = require('../src/routers/userRouter');
 // Import car's route
-const carRouters=require('../src/routers/carRouter');
+const carRouters = require('../src/routers/carRouter');
 
 const app = express();
 //Using routes for POST method
@@ -20,11 +20,11 @@ app.use(carRouters);
 
 
 
-app.listen(portNumber, () => console.log('Server listening at port ',portNumber))
+app.listen(portNumber, () => console.log('Server listening at port ', portNumber))
 
 // const bcrypt=require('bcryptjs');
-const jwt=require('jsonwebtoken');
-const myFunction=async ()=>{
+const jwt = require('jsonwebtoken');
+const myFunction = async () => {
     // const password='Imceobitch';
     // const hashedPassword=await bcrypt.hash(password,8);
 
@@ -34,8 +34,10 @@ const myFunction=async ()=>{
     // // const isMatched=await bcrypt.compare('Imceobitch1',hashedPassword);
     // // console.log(isMatched?'Password matched':'Password mismatch');    
     // console.log(await bcrypt.compare('Imceobitch1',hashedPassword)?'Password matched':'Password mismatch'); 
-    const token=jwt.sign({_id: 'abcd1234'},'hellofraands');
-    console.log(token);    
+    const token = jwt.sign({
+        _id: 'abcd1234'
+    }, 'hellofraands');
+    console.log(token);
 
 }
 myFunction();
