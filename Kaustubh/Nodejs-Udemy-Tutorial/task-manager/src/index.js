a76bbe81cd78ec1934a1136f2ce64d2309c3fe0d
kaustubhkz <kaustubh@4alpha.in>
2019-04-17 17:55:19 +0530

    Use of populate which is SQL like join operation

--- Kaustubh/Nodejs-Udemy-Tutorial/task-manager/src/index.js
+++ Kaustubh/Nodejs-Udemy-Tutorial/task-manager/src/index.js
@@ -31,7 +31,7 @@ app.use(express.json());
 // app.use((req,res,next)=>{
 //     // My code is long written, below is optimized code
 //     // const methodName=req.method;
-    
+
 //     // switch(methodName){
 //     //     case 'POST':
 //     //     case 'GET':
@@ -61,3 +61,25 @@ app.listen(portNumber, () => console.log('Server listening at port ', portNumber
 //     console.log(data);    
 // }
 // myFunction();
+const Car = require('./models/cars');
+const User = require('./models/user');
+const main = async () => {
+    const car = await Car.createNewCar.findById('5cb6b9539c318a3309bdcf3d');
+
+    // Let's derive user details using owner ID from Car model
+    // Below is less optimal code (Using await I mean)
+    // const userDetails=await User.newUser.findById(car.owner);
+    //  console.log('Car Details ',car,'\nalong with User details ',userDetails);    
+
+    //  Mongoose has a more powerful alternative called populate(), which lets you reference
+    //  documents in other collections.
+    // MongoDB has the join-like $lookup aggregation operator in versions >= 3.2.
+    // await car.populate('owner').execPopulate();
+    // console.log(car.owner);    
+
+    const userDetails=await User.newUser.findById("5cb5a879a90b0c25740c194a");
+    await userDetails.populate('cars').execPopulate();
+    console.log(userDetails.cars);    
+}
+
+main();
