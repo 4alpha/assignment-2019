ad999c580b11425c823b7749f21244d14c12e5c3
kaustubhkz <kaustubh@4alpha.in>
2019-04-18 18:24:48 +0530

    Sample code for uploading photos

--- Kaustubh/Nodejs-Udemy-Tutorial/task-manager/src/index.js
+++ Kaustubh/Nodejs-Udemy-Tutorial/task-manager/src/index.js
@@ -11,6 +11,7 @@ const carRouters = require('../src/routers/carRouter');
 const app = express();
 //Using routes for POST method
 
+
 //Configure server to parse data in JSON
 app.use(express.json());
 
@@ -47,39 +48,3 @@ app.use(userRouters);
 app.use(carRouters);
 
 app.listen(portNumber, () => console.log('Server listening at port ', portNumber))
-
-
-// const jwt = require('jsonwebtoken');
-// const myFunction = async () => {
-
-//     const token = jwt.sign({
-//         _id: 'abcd1234'
-//     }, 'hellofraands',{expiresIn: '7 days'});
-//     console.log(token);
-//     const data=jwt.verify(token,'hellofraands');
-
-//     console.log(data);    
-// }
-// myFunction();
-const Car = require('./models/cars');
-const User = require('./models/user');
-const main = async () => {
-    const car = await Car.createNewCar.findById('5cb6b9539c318a3309bdcf3d');
-
-    // Let's derive user details using owner ID from Car model
-    // Below is less optimal code (Using await I mean)
-    // const userDetails=await User.newUser.findById(car.owner);
-    //  console.log('Car Details ',car,'\nalong with User details ',userDetails);    
-
-    //  Mongoose has a more powerful alternative called populate(), which lets you reference
-    //  documents in other collections.
-    // MongoDB has the join-like $lookup aggregation operator in versions >= 3.2.
-    // await car.populate('owner').execPopulate();
-    // console.log(car.owner);    
-
-    const userDetails=await User.newUser.findById("5cb5a879a90b0c25740c194a");
-    await userDetails.populate('cars').execPopulate();
-    console.log(userDetails.cars);    
-}
-
-main();
