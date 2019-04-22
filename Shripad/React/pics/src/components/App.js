dbffa1738c0e7127c95ef31c036a823908cd5510
shripad-4alpha <shripad@4alpha.in>
2019-04-22 11:40:11 +0530

    Parent Child Communication

--- Shripad/React/pics/src/components/App.js
+++ Shripad/React/pics/src/components/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import unsplash from '../api/unsplash';
-import SearchBar from './SearchBar';
-import ImageList from './ImageList';
-
+import React from "react";
+import unsplash from "../api/unsplash";
+import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
+import Child from "./Button";
+import Ref from "./Ref"
 class App extends React.Component {
-  state = { images: []};
+  state = { images: [], text: "Welcome from Parent to Child" };
 
-   onSearchSubmit = async (term) => {
-      const response = await unsplash.get('/search/photos', {
-      params: {query : term}
-      });
-  this.setState({images : response.data.results }); 
-    }
+  onSearchSubmit = async term => {
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term }
+    });
+    this.setState({ images: response.data.results });
+    console.log(response);
+  };
+
+  handleClickParent = data => {
+    alert(data[1]);
+    // console.log("App.js");
+  };
 
   render() {
     return (
-      <div className="ui container" style={{ marginTop: '10px' }}>
+      <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        <ImageList images={this.state.images}/>
+        <Child text={this.state.text} onClick={this.handleClickParent} />
+        <ImageList images={this.state.images} />
+        <br/>
+        <Ref />
       </div>
     );
   }
