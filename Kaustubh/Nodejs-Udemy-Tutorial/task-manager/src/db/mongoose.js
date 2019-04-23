6cd10cb626a2ede645a42b43f328b514b0c0503b
kaustubhkz <kaustubh@4alpha.in>
2019-04-23 17:48:35 +0530

    Added an environment variable for dbname

--- Kaustubh/Nodejs-Udemy-Tutorial/task-manager/src/db/mongoose.js
+++ Kaustubh/Nodejs-Udemy-Tutorial/task-manager/src/db/mongoose.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 // const connectionURL = 'mongodb://127.0.0.1:27017';
 const connectionURL = process.env.DATABASE_CONNECTION_URL;
 //Let's concatenate Database name in below connectionURl
-mongoose.connect(connectionURL + '/task-manager-api', {
+const dbName=process.env.DATABASE_NAME;
+mongoose.connect(connectionURL + '/'+dbName, {
     useNewUrlParser: true,
     useCreateIndex: true,
     // Below removes deprecated warning
